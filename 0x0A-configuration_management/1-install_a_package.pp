718e7818e935585950f381073fd75e31cd5e722a
Ted KOUHOUENOU <teddams047@gmail.com>
2023-05-25 09:02:47 +0100

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# installs flask from pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
