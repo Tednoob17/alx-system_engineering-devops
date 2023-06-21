40d4d00d21e09ebdd11e10cd0326adc9f4205878
Ted KOUHOUENOU <teddams047@gmail.com>
2023-06-21 19:31:48 +0100

    💾 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,6 +5,7 @@ import requests
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit.
+
     Args:
         subreddit (str): The subreddit to search.
         word_list (list): The list of words to search for in post titles.
