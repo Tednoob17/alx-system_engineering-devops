1b5542c5db4acd7d06f54d0b3d8657112ceb3afb
Ted KOUHOUENOU <teddams047@gmail.com>
2023-06-23 10:01:13 +0100

    replica fix

--- 0x14-mysql/create_user.sql
+++
@@ -1,8 +0,0 @@
--- Create user holberton and give access to primary/replica status of the database
-CREATE USER
- IF NOT EXISTS 'holberton_user'@'localhost' 
- IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE
-  ON * . * 
-  TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
